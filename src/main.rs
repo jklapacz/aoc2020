379ef7f58bc9bc3a7066a8d0b872654c80216feb
jklapacz <kubaklapacz@gmail.com>
2020-12-02 06:22:21 +0000

    cleanup code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 use std::fs::File;
-use std::io::Read;
-use std::io::{self, BufRead, Error};
+use std::io::{Error, Read};
 use std::path::Path;
-use std::{env, fs};
-use std::assert;
-extern crate yaml_rust;
-use yaml_rust::{Yaml, YamlEmitter, YamlLoader};
+use std::fs;
+
+use yaml_rust::YamlLoader;
 
 fn main() -> Result<(), Error> {
-    let current_dir = env::current_dir()?;
-    println!(
-        "Entries modified in the last 24 hours in {:?}:",
-        current_dir
-    );
-
     for entry in fs::read_dir("inputs")? {
         if let Ok(entry) = entry {
             let c = read_lines(entry.path());
@@ -28,52 +20,17 @@ fn main() -> Result<(), Error> {
                     .filter(|s| !s.is_empty())
                     .map(|s| s.parse().unwrap())
                     .collect();
-                assert_eq!(find_sum_to_2020(&input_vector), scenario["outputs"][0].as_i64().unwrap());
-                assert_eq!(find_sum_3_to_2020(&input_vector), scenario["outputs"][1].as_i64().unwrap());
-                // println!("sum to 2020: {:?}", find_sum_to_2020(&input_vector));
-                // println!("sum 3 to 2020: {:?}", find_sum_3_to_2020(&input_vector));
-                // println!("input {:?}", scenario["input"]);
-                // println!("output {:?}", scenario["outputs"]);
+                assert_eq!(
+                    find_sum_to_2020(&input_vector),
+                    scenario["outputs"][0].as_i64().unwrap()
+                );
+                assert_eq!(
+                    find_sum_3_to_2020(&input_vector),
+                    scenario["outputs"][1].as_i64().unwrap()
+                );
             }
-            // for scenario in &docs[0]["scenarios"] {
-
-            // }
-            // println!("{:?}", &docs.unwrap()[0]["scenarios"][0]["output"]);
-            // println!("{}", c);
-            // let mut file = File::open(entry.path()).expect("could not load");
-            // let mut contents = String::new();
-
-            // file.read_to_string(&mut contents).expect("could not read");
-
-            // if let Ok(lines) = read_lines(entry.path()) {
-            //     for line in lines {
-            //         if let Ok(i) = line {
-            //             println!("{}", i);
-            //         }
-            //     }
-            // }
-            // println!("{:?}", entry.path());
-            // let buffered = BufReader::new(contents);
-            // Ok(io::BufReader::new(file).lines())
-
-            // for line in buffered.lines() {
-            // println!("{}", line)
-            // }
-            // let input = fs::File.open(entry.path())?
-            // println!("{:?}", entry)
         }
     }
-    // let path = "input/01/part_01.txt";
-
-    // let mut output = File::create(path)?;
-    // write!(output, "Rust\n💖\nFun")?;
-
-    // let input = File::open(path)?;
-    // let buffered = BufReader::new(input);
-
-    // for line in buffered.lines() {
-    //     println!("{}", line?);
-    // }
 
     Ok(())
 }
